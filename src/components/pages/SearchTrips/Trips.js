4fe0b8d33c990ae04d9bfb5e74892b74d669e6b7
Azeem Pasha <azeemshaik025@gmail.com>
2023-10-13 21:48:16 +0530

    Edited some styles and animations

--- src/components/pages/SearchTrips/Trips.js
+++ src/components/pages/SearchTrips/Trips.js
@@ -1,13 +1,18 @@
 import React, { useEffect, useRef, useState } from "react";
 import { FaLongArrowAltRight } from 'react-icons/fa';
 import { RxCross1 } from "react-icons/rx";
-import { motion } from "framer-motion";
+import { motion, useInView } from "framer-motion";
 import { TripImages } from "../../utilities/ImagesnBlurHashes";
 import ImageComponent from "../../utilities/ImageComponent";
 
 const PopUp = (props) => {
     const { name, email, phNumber, cost, availableSeats, source, destination } = props.tripInfo;
     const { stopOvers, timeDetails } = props.tripInfo;
+    const { togglePopUp } = props;
+    const popRef = useRef(null);
+    const isInView = useInView(popRef, {
+        once: true
+    })
     const [sm, setSm] = useState(true);
     useEffect(() => {
         const screenWidth = window.innerWidth;
@@ -15,8 +20,14 @@ const PopUp = (props) => {
             setSm(false);
         }
     }, [])
-    return <div className="pop-up-section">
+    return <div ref={popRef} style={{
+        scale: isInView ? 1.2 : .7,
+        opacity: isInView ? 1 : 0,
+        marginTop: isInView ? 0 : '50px',
+        transition: "all 0.7s cubic-bezier(0.17, 0.55, 0.55, 1) 0.3s"
+    }} className="pop-up-section" >
         {sm && <ImageComponent src={TripImages[2].image} blur={TripImages[2].blurHash} />}
+        <RxCross1 className="cross" onClick={togglePopUp} />
         <div className="trip-pop-up">
             <div className="single-trip-heading">
                 <span>
@@ -51,45 +62,35 @@ const PopUp = (props) => {
                 <p>{email}</p>
             </div>
         </div>
-    </div>
+    </div >
 }
 const Trips = (props) => {
     const { tripInfo } = props;
     const [showing, setShowing] = useState(false);
-    const tripRef = useRef(null);
-    async function increaseScale() {
-        const element = document.querySelectorAll(".single-trip-section");
-        element.forEach((ele) => {
-            if (ele.classList.contains("single-trip-card")) {
-                ele.classList.remove("single-trip-card");
-            }
-        });
-        setShowing(true);
-        tripRef.current.classList.add('single-trip-card');
+    async function togglePopUp() {
+        setShowing(!showing);
     }
-    return <motion.div ref={tripRef} className="single-trip-section">
-        {showing ? <RxCross1 className="cross" onClick={() => { setShowing(false); tripRef.current.classList.remove('single-trip-card'); }} /> : null}
-        {!showing ?
-            <React.Fragment>
-                {true && <ImageComponent src={TripImages[2].image} blur={TripImages[2].blurHash} />}
-                <div className="single-trip-body">
-                    <div className="single-trip-heading">
-                        <span>
-                            <h5>{tripInfo.source.substring(0, 10)}{(tripInfo.source.length > 10) && "..."}</h5>
-                            <h6>{tripInfo?.timeDetails?.start}</h6>
-                        </span>
-                        <FaLongArrowAltRight />
-                        <span>
-                            <h5>{tripInfo.destination.substring(0, 10)}{(tripInfo.source.length > 10) && "..."}</h5>
-                            <h6>{tripInfo?.timeDetails?.end}</h6>
-                        </span>
-                    </div>
-                    <h5 className="date">{tripInfo?.timeDetails?.tripDate}</h5>
-                    <button onClick={increaseScale}>Get details</button>
+    return <motion.div className="single-trip-section">
+        {showing && <PopUp tripInfo={tripInfo} togglePopUp={togglePopUp} />}
+        {showing && <div className="overlay"></div>}
+        <React.Fragment>
+            <ImageComponent src={TripImages[2].image} blur={TripImages[2].blurHash} />
+            <div className="single-trip-body">
+                <div className="single-trip-heading">
+                    <span>
+                        <h5>{tripInfo.source.substring(0, 10)}{(tripInfo.source.length > 10) && "..."}</h5>
+                        <h6>{tripInfo?.timeDetails?.start}</h6>
+                    </span>
+                    <FaLongArrowAltRight />
+                    <span>
+                        <h5>{tripInfo.destination.substring(0, 10)}{(tripInfo.source.length > 10) && "..."}</h5>
+                        <h6>{tripInfo?.timeDetails?.end}</h6>
+                    </span>
                 </div>
-            </React.Fragment> :
-            <PopUp tripInfo={tripInfo} />
-        }
+                <h5 className="date">{tripInfo?.timeDetails?.tripDate}</h5>
+                <button onClick={togglePopUp}>Get details</button>
+            </div>
+        </React.Fragment>
     </motion.div >
 }
 export default Trips;
